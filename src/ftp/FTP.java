c23ac18a36ced426abaefcc19667742bb807797a
Adam Aron Edelsten <adamedelsten@hotmail.com>
2019-02-19 15:41:25 +0100

    Tæller fra 0-9

--- src/ftp/FTP.java
+++ src/ftp/FTP.java
@@ -8,6 +8,11 @@ public class FTP {
     public static void main(String[] args) {
         System.out.println("det her er en ændring");
         System.out.println("det her er en anden ændring");
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        
     }
     
 }
